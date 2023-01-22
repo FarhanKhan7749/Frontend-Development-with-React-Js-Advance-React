8e95d84504449c366c7849ba060e4dd01a98a7f0
FarhanKhan7749 <fk828633@gmail.com>
2023-01-22 09:36:27 +0530

    Tast 7 Submitting the forms(Task 5 and Task 6 init)

--- src/Components/NewExpenses/ExpenseForm.js
+++ src/Components/NewExpenses/ExpenseForm.js
@@ -1,39 +1,76 @@
-//import React, { useState } from "react";
+import React, { useState } from "react";
 import './ExpenseForm.css'
 const ExpenseForm = (props) =>{
-    // const [title,setTitle] = useState('');
-    // const [amount,setAmount] = useState('');
-    // const [date,setDate] = useState('');
+    // Task 5 in advance React.js "Lets introduce state inside the new Component" 
+    const [title,setTitle] = useState('');
+    const [amount,setAmount] = useState('');
+    const [date,setDate] = useState('');
 
-    const changeTitle = (event) => {
-        event.preventDefault();
-        console.log(event.target.value);
+
+    // Task 6 in advance react.js "Design patterns with state";
+    // const [userInput, sateInput] = useState({
+    //     title : '',
+    //     amount : '',
+    //     date : '',
+    // })
+
+    const enteredTitle  = (event) => {
+        setTitle(event.target.value);
+        // sateInput({
+        //     ...userInput,
+        //     title : event.target.value,
+        // })
+
     }
 
-    const changeAmount = (event) => {
-        event.preventDefault();
-        console.log(event.target.value);
+    const enteredAmount  = (event) => {
+        setAmount(event.target.value);
+        // sateInput({
+        //     ...userInput,
+        //     amount : event.target.value,
+        // })
+
     }
 
-    const changeDate = (event) => {
-        event.preventDefault();
-        console.log(event.target.value);
+    const enteredDate  = (event) => {
+        setDate(event.target.value);
+        // sateInput({
+        //     ...userInput,
+        //     date : event.target.value,
+        // })
+        //
+        // right way to wight the object as value in useState
+        // sateInput((prevState)=>{
+        //     return {...prevState, title: event.target.value};
+        // })
     }
     
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const enterData = {
+            title:title,
+            amount:amount,
+            date:new Date(date)
+        };
+        console.log(enterData);
+        setTitle("");
+        setAmount("");
+        setDate("");
+    }
     return <div>
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control label">
                     <label>Expense Title</label>
-                    <input type = 'text' onChange={changeTitle} />
+                    <input type = 'text' value={title} onChange={enteredTitle} />
                 </div>
                 <div className="new-expense__control label">
                     <label>Expense Amount</label>
-                    <input type = 'text' min='0.01' step='0.01' onChange={changeAmount}/>
+                    <input type = 'text' min='0.01' step='0.01' value={amount} onChange={enteredAmount}/>
                 </div>
                 <div className="new-expense__control label">
                     <label>Date</label>
-                    <input type = 'date' min='2019-01-01' max='2023-12-31' onChange={changeDate}/>
+                    <input type = 'date' min='2019-01-01' max='2023-12-31' value={date} onChange={enteredDate}/>
                 </div>
             </div>
             <div className="new-expense__actions">
