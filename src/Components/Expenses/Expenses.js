67af51015f151365e2fc4b6dd4fcef83b57dbb8b
FarhanKhan7749 <fk828633@gmail.com>
2023-01-23 23:49:37 +0530

    Conditional Rendering

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
-import ExpenseItems from "./ExpenseItems";
 import './Expenses.css';
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import { useState } from "react";
+import ExpensesList from "./ExpensesList";
 const Expense = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
 
@@ -13,10 +13,12 @@ const Expense = (props) => {
     const filterByYear = props.items.filter( expense => {
         return expense.date.getFullYear().toString() === filteredYear;
     })
+
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChnageHnadler} />
-            {filterByYear.map((expense) => (
+            {/* one method to wright the conditional satement in react. */}
+            {/* {filterByYear.length === 0 ? (<p>No expense found</p>) :filterByYear.map((expense) => (
                 <ExpenseItems
                     key = {expense.id}
                     title={expense.title}
@@ -24,7 +26,19 @@ const Expense = (props) => {
                     amount={expense.amount}
                     date={expense.date}
                 />
-            ))}
+            ))} */}
+            {/* Another method to use conditional satement in react */}
+            {/* {filterByYear.length === 0 && <p>No expense found</p>}
+            {filterByYear.length > 0 && filterByYear.map((expense) => (
+                <ExpenseItems
+                    key = {expense.id}
+                    title={expense.title}
+                    //LocationOfExpenditure={prop.LocationOfExpenditure}
+                    amount={expense.amount}
+                    date={expense.date}
+                />
+            ))} */}
+            <ExpensesList items={filterByYear} />
         </Card>
     );
 }
